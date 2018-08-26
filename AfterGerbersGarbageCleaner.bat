2daeeec84efd0ed13ea1153c12d5eec689c1e55b
vehar <vladdxmel@gmail.com>
2018-08-26 23:43:28 +0300

    final

---
+++ AfterGerbersGarbageCleaner.bat
@@ -0,0 +1,14 @@
+del "*.bak" /f /s /a: H
+
+::Altium
+rd __Previews  /s /q
+rd Project Logs*  /s /q
+del "*.LOG" /f /s /a: H
+del "*.LDP" /f /s /a: H
+del "*.RUL" /f /s /a: H
+del "*.APR_LIB" /f /s /a: H
+del "Status Report.Txt" /f /s /a: H
+sleep 90
+
+
+
